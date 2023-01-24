f2c4893328d5dd54467b480eac35e752c3077475
StuartCowley <voidstalker@live.co.uk>
2023-01-24 13:48:40 +0000

    Fix broken test, install react test renderer and snapshot App

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,19 @@
 import React, { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import App from "../components/App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText("Intro to React II");
+  const linkElement = screen.getByText(/Testing in React/);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders as expected", () => {
+  const testRenderer = renderer
+    .create(
+      <App />
+    )
+    .toJSON();
+
+  expect(testRenderer).toMatchSnapshot();
 });
